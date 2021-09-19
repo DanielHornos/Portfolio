4acde2bd96eff867064eb38044fafd36877e2b89
DanielHornos <daniel03hv@gmail.com>
2021-09-19 14:06:54 +0200

    Added logo and css style to topbar

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,9 +1,17 @@
 import "./topbar.scss"
 
-export default function Topbar(){
+export default function Topbar() {
     return (
         <div className="topbar" id="topbar">
-        topbar
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro" className="logo">
+                        Daniel Hornos
+                    </a>
+                </div>
+                <div className="right">
+                </div>
+            </div>
         </div>
     )
 }
