9773242422a2cb418b20c3edbd6ebf4c93437e54
DanielHornos <daniel03hv@gmail.com>
2021-09-21 00:30:22 +0200

    Added ityped module and design intro component

--- src/components/intro/intro.jsx
+++ src/components/intro/intro.jsx
@@ -1,10 +1,41 @@
-import "./intro.scss"
+import React, { useEffect, useRef } from "react";
+import { init } from "ityped";
+
+import introImage from '../../assets/daniel portfolio.png';
+
+import "./intro.scss";
 
 export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Javascript", "React", "Redux"],
+        });
+    }, []);
+
     return (
         <div className="intro" id="intro">
-            intro
+            <div className="left">
+                <div className="imgContainer">
+                    <img src={introImage} alt="" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm</h2>
+                    <h1>Daniel Hornos Valiente</h1>
+                    <h3>
+                        Web Developer <span ref={textRef}></span>
+                    </h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt="" />
+                </a>
+            </div>
         </div>
-    )
+    );
 }
-
