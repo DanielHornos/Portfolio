0dfe5e9c7ab6b1c83235c80ffc696623274d4608
DanielHornos <daniel03hv@gmail.com>
2021-09-22 00:44:36 +0200

    Added skills logos

--- src/components/skills/skills.jsx
+++ src/components/skills/skills.jsx
@@ -1,9 +1,65 @@
+import { useTranslation } from 'react-i18next';
+
+import typescriptIcon from "../../assets/logos/icon-typescript.svg"
+import javascriptIcon from "../../assets/logos/icon-javascript.svg"
+import css3Icon from "../../assets/logos/icon-css3.svg"
+import html5Icon from "../../assets/logos/icon-html-5.svg"
+import reactIcon from "../../assets/logos/icon-react.svg"
+import sassIcon from "../../assets/logos/icon-sass.svg"
+import reduxIcon from "../../assets/logos/icon-redux.svg"
+
 import "./skills.scss"
 
-export default function Skills(){
+export default function Skills() {
+    const { t } = useTranslation();
+
+    const skills = {
+        icons: [
+            {
+                name: "HTML 5",
+                icon: html5Icon,
+            },
+            {
+                name: "CSS 3",
+                icon: css3Icon,
+            },
+            {
+                name: "TypeScript",
+                icon: typescriptIcon,
+            },
+            {
+                name: "JavaScript",
+                icon: javascriptIcon,
+            },
+            {
+                name: "React",
+                icon: reactIcon,
+            },
+            {
+                name: "Sass",
+                icon: sassIcon,
+            },
+            {
+                name: "Redux",
+                icon: reduxIcon,
+            },
+        ]
+    };
+    var skillsList = skills.icons.map(function (skills, i) {
+        return (
+            <div className="skill-tile">
+                <img src={skills.icon} alt="" className="skill-logo" />
+                {skills.name}
+            </div>
+        );
+    });
+
     return (
-        <div className="skills" id="skills">
-        skills
-        </div>
+        <section className="skills" id="skills">
+            <h1>{t("menu.skills")}</h1>
+            <div className="skills-container">
+                {skillsList}
+            </div>
+        </section>
     )
 }
